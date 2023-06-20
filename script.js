f1a0506c7162a0c2a890b51a95562d7e55f85120
Saurabh3797 <128299348+Saurabh3797@users.noreply.github.com>
2023-06-20 16:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+
+let table = {
+    "2" : "abc",
+    "3" : "def",
+    "4" : "ghi",
+    "5" : "jkl",
+    "6" : "mno",
+    "7" : "pqrs",
+    "8" : "tuv",
+    "9" : "wxyz"
+}
+
+let solutions = [] ;
+
+function solve(currentIndex, calculatedString) {
+    if(input.length <= currentIndex){
+        solutions.push(calculatedString);
+        return;
+    }
+    // currentIndex = 2, calculatedString = "dm"
+    let currentChar = input[currentIndex] // "3"
+    // table["3"] = "def"
+    for(let i = 0 ; i < table[currentChar].length; i++) {
+        // "def" 
+        solve(currentIndex + 1, calculatedString + table[currentChar][i] )
+        // solve(0 + 1, "" + "d")
+    }
+}
+
+solve(0, "")
+
+return solutions ;
+}
+
+module.exports = letterCombinations;
 }
 
 module.exports = letterCombinations;
